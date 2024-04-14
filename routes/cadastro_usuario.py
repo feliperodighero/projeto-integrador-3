f77e986632a8a2058135b8717da78784a8b47e91
felipezarichta <fezarichta@hotmail.com>
2024-04-14 14:12:22 -0300

    Configuração da rota para cadastro de usuários e conexão com o banco para executar o INSERT do cadastro, rotas do front-end corrigidas, e exemplos de querys com a tabela Fruta removidas

--- routes/cadastro_usuario.py
+++ routes/cadastro_usuario.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from database.models.cadastro_usuario import cadastrar_usuario
 
 cadastro_usuario = Blueprint('cadastro_usuario', __name__)
 
 @cadastro_usuario.route('/cadastro_usuario', methods=['GET', 'POST'])
 def cadastro_usuario_route():
+    if request.method == 'POST':
+        nome = request.form['UserName']
+        senha = request.form['UserConfirmPassword']
+        cpf = request.form['UserCpf']
+        telefone = request.form['UserNumberPhone']
+        data_nascimento = request.form['UserDateBirth']
+        crm = request.form['UserCrm']
+        cargo = request.form['UserCodeCarg']
+        rua = request.form['UserStreet']
+        numero_casa = request.form['UserNumberHouse']
+        bairro = request.form['UserNeighborhood']
+        complemento = request.form['UserComplement']
+        data_cadastro = request.form['UserDateRegister']
+        cadastrar_usuario(nome, senha, cpf, telefone, data_nascimento, crm, cargo, rua, numero_casa, bairro, complemento, data_cadastro)
+        return(redirect(url_for('cadastro_usuario.cadastro_usuario_route')))
     return render_template('cadastro_usuario.html')
--- routes/home.py
+++ routes/home.py
@@ -1,19 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from database.models.fruta import cadastro_fruta, buscar_frutas
+from flask import Blueprint, render_template
 
 
 home_route = Blueprint('home', __name__)
 
-@home_route.route('/', methods=['GET', 'POST'])
+@home_route.route('/', methods=['GET'])
 def home():
-    if request.method == 'POST':
-        fruitName = request.form['fruitName']
-        fruitQuantity = request.form['fruitQuantity']
-        cadastro_fruta(fruitName, fruitQuantity)
-        return redirect(url_for('home.home'))
     return render_template('home.html')
-
-@home_route.route('/listar_frutas', methods=['GET'])
-def listar_frutas():
-    frutas = buscar_frutas()
-    return render_template('listar_frutas.html', frutas=frutas)
