3845333a679887f64bccd071ddce3663602ac421
felipezarichta <fezarichta@hotmail.com>
2024-04-19 19:52:57 -0300

    Identacao

--- database/models/cadastro_usuario.py
+++ database/models/cadastro_usuario.py
@@ -1,14 +1,50 @@
 from sqlalchemy.orm import sessionmaker
 from database.config import engine
 from sqlalchemy import text
+from werkzeug.security import generate_password_hash
+
 
 Session = sessionmaker(bind=engine)
 
-def cadastrar_usuario(nome, senha, cpf, telefone, data_nascimento, crm, cargo, rua, numero_casa, bairro, complemento, data_cadastro, status_usuario = True):
+
+def cadastrar_usuario(
+    nome,
+    senha,
+    cpf,
+    telefone,
+    data_nascimento,
+    crm,
+    cargo,
+    rua,
+    numero_casa,
+    bairro,
+    complemento,
+    data_cadastro,
+    status_usuario=True,
+):
     session = Session()
     try:
-        query = text("INSERT INTO USUARIO (NM_USU, SENHA, CRM, CPF, CD_CARGO, STATUS_USU, TELEFONE, BAIRRO, RUA, NUMERO_CASA, COMPLEMENTO, DT_NASC, DT_CAD) VALUES (:NM_USU, :SENHA, :CRM, :CPF, :CD_CARGO, :STATUS_USU, :TELEFONE, :BAIRRO, :RUA, :NUMERO_CASA, :COMPLEMENTO, :DT_NASC, :DT_CAD) ")
-        session.execute(query, {'NM_USU': nome, 'SENHA': senha, 'CRM': crm, 'CPF': cpf, 'CD_CARGO': cargo, 'STATUS_USU': status_usuario, 'TELEFONE': telefone, 'BAIRRO': bairro, 'RUA': rua, 'NUMERO_CASA': numero_casa, 'COMPLEMENTO': complemento, 'DT_NASC': data_nascimento, 'DT_CAD': data_cadastro})
+        query = text(
+            "INSERT INTO USUARIO (NM_USU, SENHA, CRM, CPF, CD_CARGO, STATUS_USU, TELEFONE, BAIRRO, RUA, NUMERO_CASA, COMPLEMENTO, DT_NASC, DT_CAD) VALUES (:NM_USU, :SENHA, :CRM, :CPF, :CD_CARGO, :STATUS_USU, :TELEFONE, :BAIRRO, :RUA, :NUMERO_CASA, :COMPLEMENTO, :DT_NASC, :DT_CAD) "
+        )
+        session.execute(
+            query,
+            {
+                "NM_USU": nome,
+                "SENHA": senha,
+                "CRM": crm,
+                "CPF": cpf,
+                "CD_CARGO": cargo,
+                "STATUS_USU": status_usuario,
+                "TELEFONE": telefone,
+                "BAIRRO": bairro,
+                "RUA": rua,
+                "NUMERO_CASA": numero_casa,
+                "COMPLEMENTO": complemento,
+                "DT_NASC": data_nascimento,
+                "DT_CAD": data_cadastro,
+            },
+        )
         session.commit()
     except:
         session.rollback()
