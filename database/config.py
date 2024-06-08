1873b7f52494b47e26111307cfbd652ac61d09ac
feliperodighero <feliperzarichta@gmail.com>
2024-06-07 21:39:29 -0300

    Alinhamento nas informações da ordem de serviço

--- database/config.py
+++ database/config.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 
 # Cria a conexão com o banco de dados
-engine = create_engine('mssql+pyodbc://@Felipe/PRODUCAO?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
-
+engine = create_engine(
+    "mssql+pyodbc://@Felipe/PRODUCAO?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server"
+)
