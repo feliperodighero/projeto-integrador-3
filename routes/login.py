eba142b48bdb2f9a24ffccc6ef8f50fb867b150a
feliperodighero <feliperzarichta@gmail.com>
2024-06-11 17:54:16 -0300

    Corrigindo a validação do login, para dar erro somente quando o usuário digita o código ou a senha errada

--- routes/login.py
+++ routes/login.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, redirect, render_template, request, url_for, flash
+from flask import Blueprint, redirect, render_template, request, url_for, flash, session
 from flask_login import login_required, login_user, logout_user
 
 from database.models.login import login_authenticate
 
 login_user = Blueprint("login_user", __name__)
+
 
 @login_user.route("/login", methods=["GET", "POST"])
 def login():
@@ -11,16 +12,21 @@ def login():
         username = request.form["login"]
         password = request.form["password"]
         user = login_authenticate(username, password)
+        session["post_made"] = True
         if user:
             if user[6] == 1:
                 return redirect(url_for("home.home"))
             else:
-                flash("Sua conta está desativada. Entre em contato com o administrador.", "error")
+                flash(
+                    "Sua conta está desativada. Entre em contato com o administrador.",
+                    "error",
+                )
                 return redirect(url_for("login_user.login"))
         else:
             flash("Nome de usuário ou senha incorretos.", "error")
             return redirect(url_for("login_user.login"))
     return render_template("login_page.html")
+
 
 @login_user.route("/logout")
 @login_required
